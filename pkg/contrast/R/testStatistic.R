de4f0d20b54535bc8f700b40d8b172f5a3d4bd67
kuhn (Dr. ) <mxkuhn@gmail.com>
2011-07-31 19:06:24 +0000

    updated with additional error trapping for lme objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/contrast@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/contrast/R/testStatistic.R
+++ pkg/contrast/R/testStatistic.R
@@ -1,34 +1,36 @@
-# this is a utility function that is called from contrast.lm
+## this is a utility function that is called from contrast.lm
 testStatistic <- function(fit, designMatrix, critVal, params=getCoefficients(fit), covMatrix=vcov(fit))
 {
-   est <- drop(designMatrix %*% params)
-   v <- drop((designMatrix %*% covMatrix) %*% t(designMatrix))
-   ndf <- if (is.matrix(v)) nrow(v) else 1
-   se <- if (ndf == 1) sqrt(v) else sqrt(diag(v))
-   testStat <- est / se
+  est <- drop(designMatrix %*% params)
+  v <- drop((designMatrix %*% covMatrix) %*% t(designMatrix))
+  ndf <- if (is.matrix(v)) nrow(v) else 1
+  se <- if (ndf == 1) sqrt(v) else sqrt(diag(v))
+  testStat <- est / se
 
-   # this is inconsistent theoretically, but consistent with each R model
-   df <- switch(
-      class(fit)[1],
-      lm =, glm = fit$df.residual,
-      gls = fit$dims$N -  fit$dims$p,
-      lme = fit$dims$N -  length(params) - ncol(fit$apVar),      
-      geese = NA)
-   
-   P <- switch(
-      class(fit)[1],
-      lm =, glm = 2 * (1 - pt(abs(testStat), df)),
-      gls = 2 * (1 - pt(abs(testStat), df)),
-      lme = 2 * (1 - pt(abs(testStat), df)),      
-      geese = 2 * (1 - pnorm(abs(testStat))))
+  if(class(fit)[1] == "lme" && class(fit$apVar)[1] == "character") stop(fit$apVar)
+  
+  ## this is inconsistent theoretically, but consistent with each R model
+  df <- switch(
+               class(fit)[1],
+               lm =, glm = fit$df.residual,
+               gls = fit$dims$N -  fit$dims$p,
+               lme = fit$dims$N -  length(params) - ncol(fit$apVar),      
+               geese = NA)
+  
+  P <- switch(
+              class(fit)[1],
+              lm =, glm = 2 * (1 - pt(abs(testStat), df)),
+              gls = 2 * (1 - pt(abs(testStat), df)),
+              lme = 2 * (1 - pt(abs(testStat), df)),      
+              geese = 2 * (1 - pnorm(abs(testStat))))
 
-   list(Contrast=est,
-        SE=se,
-        Lower=est - critVal * se,
-        Upper=est + critVal * se,
-        testStat=testStat,
-        df = df,
-        Pvalue=P,
-        var=v,
-        X=designMatrix)
+  list(Contrast=est,
+       SE=se,
+       Lower=est - critVal * se,
+       Upper=est + critVal * se,
+       testStat=testStat,
+       df = df,
+       Pvalue=P,
+       var=v,
+       X=designMatrix)
 }
